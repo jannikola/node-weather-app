192ffec168881afa9b69c2073f887e49c4d5bf10
jannikola <jannikola@gmail.com>
2020-01-22 22:43:15 +0100

    request modified

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const request = require('request')
 const url = 'https://api.darksky.net/forecast/beaec2f869efeb5defa92aef9802bf71/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.currently)
 })
