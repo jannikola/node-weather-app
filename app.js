641a4ddf15960dad81e0a656a34872290b6155e6
jannikola <jannikola@gmail.com>
2020-01-26 12:47:42 +0100

    geocode commented code remeoved

--- app.js
+++ app.js
@@ -13,20 +13,6 @@ const geocode = require('./utils/geocode')
 //     }
 // })
 
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiamFubmlrb2xhIiwiYSI6ImNrNXV2M3M5bTE5amczZ3BiNTVoaXl1Z3kifQ.qx9aQsFFLA3DxUYnXHSz9A'
-
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to location services!')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find location. Try another search.')
-//     } else {
-//         const latitude = response.body.features[0].center[0]
-//         const longitude = response.body.features[0].center[1]
-//         console.log(latitude, longitude)
-//     }
-// })
-
 geocode('sarajevo', (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
